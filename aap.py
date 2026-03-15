9ef128badf75c7607b283e8d2cdcf62b8f60e7be
Your Name <you@example.com>
2026-03-15 13:49:54 +0530

    added secret

--- aap.py
+++ aap.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 st.title("Harshit's AI Chatbot 🤖")
 

20b63ca6964c3495385790389e42fc8b82a1b529
Your Name <you@example.com>
2026-03-15 13:44:47 +0530

    remove env secret

--- aap.py
+++ aap.py
@@ -1,31 +1,43 @@
 import streamlit as st
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 st.title("Harshit's AI Chatbot 🤖")
 
-# memory
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# show previous messages
+# show previous chat
 for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.write(msg["content"])
 
-# user input
 prompt = st.chat_input("Ask something...")
 
 if prompt:
 
-    # show user message
+    st.session_state.messages.append(
+        {"role": "user", "content": prompt}
+    )
+
     with st.chat_message("user"):
         st.write(prompt)
 
-    # fake AI response
-    response = "You said: " + prompt
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=st.session_state.messages
+    )
+
+    reply = response.choices[0].message.content
+
+    st.session_state.messages.append(
+        {"role": "assistant", "content": reply}
+    )
 
     with st.chat_message("assistant"):
-        st.write(response)
-
-    # save messages
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        st.write(reply)
--- main.py
+++ main.py
@@ -1,6 +1,43 @@
-def main():
-    print("Hello from streamlit-project!")
+import streamlit as st
+from groq import Groq
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+st.title("Harshit's AI Chatbot 🤖")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# show previous chat
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
+
+prompt = st.chat_input("Ask something...")
+
+if prompt:
+
+    st.session_state.messages.append(
+        {"role": "user", "content": prompt}
+    )
+
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=st.session_state.messages
+    )
+
+    reply = response.choices[0].message.content
+
+    st.session_state.messages.append(
+        {"role": "assistant", "content": reply}
+    )
+
+    with st.chat_message("assistant"):
+        st.write(reply)

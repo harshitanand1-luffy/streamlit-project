5f4a1a9ee6f51af0fff4fb11e5f5f57f965f2cd8
Your Name <you@example.com>
2026-03-15 13:29:27 +0530

    added chatbot UI

--- aap.py
+++ aap.py
@@ -1,4 +1,31 @@
 import streamlit as st
 
-st.title("Meowwwwwwwwwwwwwwwwww")
-st.write("Hello Bhai Log")
+st.title("Harshit's AI Chatbot 🤖")
+
+# memory
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# show previous messages
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
+
+# user input
+prompt = st.chat_input("Ask something...")
+
+if prompt:
+
+    # show user message
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    # fake AI response
+    response = "You said: " + prompt
+
+    with st.chat_message("assistant"):
+        st.write(response)
+
+    # save messages
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "assistant", "content": response})
